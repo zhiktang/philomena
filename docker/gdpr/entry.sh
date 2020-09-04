bfb35d39f1a8e27aa24d90a5034c5948e996dbd5
Chase22 <chase@mailbox.org>
2020-09-04 09:28:40 +0200

    Remove log lines

--- docker/gdpr/entry.sh
+++ docker/gdpr/entry.sh
@@ -7,13 +7,6 @@ export PGDATABASE=$POSTGRES_DB
 export PGUSER=$POSTGRES_USER
 export PGPASSWORD=$POSTGRES_PASSWORD
 
-#print environment
-echo "Started with psql config"
-echo "PGHOST=$PGHOST"
-echo "PGPORT=$PGPORT"
-echo "PGDATABASE=$PGDATABASE"
-echo "PGUSER=$PGUSER"
-
 # start cron
 echo "Starting cron"
 /usr/sbin/crond -f -l 8
