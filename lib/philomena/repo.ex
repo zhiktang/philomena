3e360d1c7c3daa209c29fb40a82dfc2e6e0661dc
byte[] <byteslice@airmail.cc>
2020-09-06 12:39:53 -0400

    remove Repo.isolated_transaction/2
    Cherry-picked https://github.com/philomena-dev/philomena/commit/2913da269203a02e5d049708311a6e92baee49c6
    
    Co-Authored-By: liamwhite <9658600+liamwhite@users.noreply.github.com>

--- lib/philomena/repo.ex
+++ lib/philomena/repo.ex
@@ -1,17 +1,10 @@
 defmodule Philomena.Repo do
-  alias Ecto.Multi
 
   use Ecto.Repo,
     otp_app: :philomena,
     adapter: Ecto.Adapters.Postgres
 
   use Scrivener, page_size: 250
-
-  @levels %{
-    read_committed: "READ COMMITTED",
-    repeatable_read: "REPEATABLE READ",
-    serializable: "SERIALIZABLE"
-  }
 
   def init(_type, config) do
     config = Keyword.put(config, :username, System.get_env("POSTGRES_USER"))
@@ -25,17 +18,5 @@ defmodule Philomena.Repo do
     #config = Keyword.put(config, :username, Application.get_env(:philomena, Philomena, :username))
     #config = Keyword.put(config, :password, Application.get_env(:philomena, Philomena, :password))
     {:ok, config }
-  end
-
-  def isolated_transaction(%Multi{} = multi, level) do
-    Multi.append(
-      Multi.new()
-      |> Multi.run(:isolate, fn repo, _chg ->
-        repo.query!("SET TRANSACTION ISOLATION LEVEL #{@levels[level]}")
-        {:ok, nil}
-      end),
-      multi
-    )
-    |> Philomena.Repo.transaction()
   end
 end
